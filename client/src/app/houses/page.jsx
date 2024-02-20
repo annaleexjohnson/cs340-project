593e16cca6c901c723f4619a6423d3629f1b9295
Annalee X. Johnson <annaleexjohnson@gmail.com>
2024-02-20 16:05:04 -0600

    implement select query for houses page

--- client/src/app/houses/page.jsx
+++ client/src/app/houses/page.jsx
@@ -5,27 +5,21 @@ import AddHouse from "../components/addHouse/AddHouse";
 import { MdDeleteForever } from "react-icons/md";
 
 export default function Houses() {
-  // const [houseInfo, setHouseInfo] = useState({});
+  const [houseInfo, setHouseInfo] = useState({});
 
   // fetch house names on page load
-  // useEffect(() => {
-  //   const fetchHouseInfo = async () => {
-  //     const res = await fetch("/api/houseInfo", {
-  //       method: "GET",
-  //     });
-  //     const houses = await res.json();
-  //     setHouseInfo(houses);
-  //   };
+  useEffect(() => {
+    const fetchHouseInfo = async () => {
+      const res = await fetch("/api/houseInfo", {
+        method: "GET",
+      });
+      const houses = await res.json();
+      console.log(houses);
+      setHouseInfo(houses);
+    };
 
-  //   fetchHouseInfo();
-  // }, []);
-
-  const houseInfo = [
-    { houseName: "Gryffindor", houseFounder: "Godric Gryffindor" },
-    { houseName: "Hufflepuff", houseFounder: "Helga Hufflepuff" },
-    { houseName: "Ravenclaw", houseFounder: "Rowena Ravenclaw" },
-    { houseName: "Slytherin", houseFounder: "Salazar Slytherin" },
-  ];
+    fetchHouseInfo();
+  }, []);
 
   const HouseRow = ({ name, founder }) => {
     return (
@@ -43,32 +37,34 @@ export default function Houses() {
 
   return (
     <>
-      <div className={styles.container}>
-        <h1>Hogwarts Houses</h1>
-        <div className={styles.displayHousesContainer}>
-          <h3>List of Houses:</h3>
-          <table className={styles.displayHousesTable}>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Founder</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {houseInfo.map((house, index) => {
-                return (
-                  <HouseRow
-                    name={house.houseName}
-                    founder={house.houseFounder}
-                  />
-                );
-              })}
-            </tbody>
-          </table>
+      {houseInfo && (
+        <div className={styles.container}>
+          <h1>Hogwarts Houses</h1>
+          <div className={styles.displayHousesContainer}>
+            <h3>List of Houses:</h3>
+            <table className={styles.displayHousesTable}>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Founder</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {houseInfo.map((house, index) => {
+                  return (
+                    <HouseRow
+                      name={house.house_name}
+                      founder={house.house_founder}
+                    />
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+          <AddHouse />
         </div>
-        <AddHouse />
-      </div>
+      )}
     </>
   );
 }
