4d918b593337ccdb5bd1ab6bd352d524270bb76b
Annalee X. Johnson <annaleexjohnson@gmail.com>
2024-02-22 15:16:17 -0600

    comment out wizard info api

--- client/src/app/api/wizardInfo/route.js
+++ client/src/app/api/wizardInfo/route.js
@@ -1,16 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
-import pool from "../db-connector/route.js";
-const db = pool;
+// import { NextRequest, NextResponse } from "next/server";
+// import pool from "../db-connector/route.js";
+// const db = pool;
 
-// Get list of wizards
-export async function GET(req) {
-  const selectQuery =
-    "SELECT Wizards.wizard_name, Wizards.wizard_graduated, Houses.house_name FROM Wizards, Houses WHERE Wizards.wizard_house = Houses.house_id GROUP BY Wizards.wizard_name;";
+// // Get list of wizards
+// export async function GET(req) {
+//   const selectQuery =
+//     "SELECT Wizards.wizard_name, Wizards.wizard_graduated, Houses.house_name FROM Wizards, Houses WHERE Wizards.wizard_house = Houses.house_id GROUP BY Wizards.wizard_name;";
 
-  const conn = await db.getConnection();
-  const wizards = await conn.query(selectQuery);
-  console.log(wizards);
-  conn.release();
+//   const conn = await db.getConnection();
+//   const wizards = await conn.query(selectQuery);
+//   console.log(wizards);
+//   conn.release();
 
-  return Response.json(wizards);
-}
+//   return Response.json(wizards);
+// }
